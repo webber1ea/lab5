8fc3a268a97789d82ecb5bb4b7ec797a029393bd
Emily Webber <webber.emilya@gmail.com>
2017-06-30 09:04:05 -0400

    this is javascript lab5

---
+++ fahrenheit.js
@@ -0,0 +1,8 @@
+function fToC(fahrenheit)
+{
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+}
+fToC(100);
